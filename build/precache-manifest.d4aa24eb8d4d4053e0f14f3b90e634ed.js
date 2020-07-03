773bcd7bad8fa4545e43c2162d57cbce3f208774
Jozhus <jozhua4321@yahoo.com>
2020-07-02 17:25:30 -0700

    reactapp setup

---
+++ build/precache-manifest.d4aa24eb8d4d4053e0f14f3b90e634ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c694e586f62623893b97019b7d8733c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f92b515c64545fee5d5",
+    "url": "/static/js/2.3df7ef3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3df7ef3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5977f8c044b35dc63eba",
+    "url": "/static/js/main.d62d54e3.chunk.js"
+  },
+  {
+    "revision": "8179a1eac53989d25e76",
+    "url": "/static/js/runtime-main.b00297d7.js"
+  }
+]);
---
+++ src/Game.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Game extends Component {
+    constructor(props: {}) {
+        super(props);
+    }
+
+    public render(): JSX.Element {
+        return (
+            <React.Fragment>
+
+            </React.Fragment>
+        );
+    }
+}
+
+export { Game };
