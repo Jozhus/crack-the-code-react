b519e9b0bf8c1baa26e0555f117d2b9b57ff5aa8
Jozhus <vauit3@yahoo.com>
2022-01-21 18:14:18 -0800

    Updates

---
+++ precache-manifest.c607231b204da5d83eaa00c02b090221.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c4a4912c9b0c1d1fbe2d71185bc82e",
+    "url": "/crack-the-code-react/index.html"
+  },
+  {
+    "revision": "09cdf3ff0d5815253516",
+    "url": "/crack-the-code-react/static/js/2.354efaea.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/crack-the-code-react/static/js/2.354efaea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35d4a76e4f06c65750e8",
+    "url": "/crack-the-code-react/static/js/main.f651362b.chunk.js"
+  },
+  {
+    "revision": "4cf35fb36b0e08b20d00",
+    "url": "/crack-the-code-react/static/js/runtime-main.0d4a7366.js"
+  }
+]);
